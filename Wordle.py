6ac59f52a380fae6208763b4fb575cbef7b7424d
mqi-cs <mqasimimran@outlook.com>
2025-03-11 12:46:15 +0000

    Fixed variable names and commentds

--- Wordle.py
+++ Wordle.py
@@ -6,11 +6,11 @@ pygame.init()           #initialising pygame
 screen_width,screen_height = 600,700    #variables for screen width and height
 
 # Define colours
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-GREY = (128, 128, 128)
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Green = (0, 255, 0)
+Yellow = (255, 255, 0)
+Grey = (128, 128, 128)
 
 screen = pygame.display.set_mode((screen_width,screen_height))   #initialising display window
 
@@ -22,10 +22,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(WHITE)  # Fill the screen with white
+    screen.fill(Black)  # Fill the screen with white
 
     # Draw a black rectangle
-    pygame.draw.rect(screen, BLACK, (100, 100, 200, 100))  # (x , y cooridnates, width, height)
+    pygame.draw.rect(screen, White, (100, 100, 200, 100))  # (x , y cooridnates, width, height)
 
     pygame.display.flip()  # Update the screen
 
