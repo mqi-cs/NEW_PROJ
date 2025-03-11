b11acf4bbeac6c181e392fc3793d7bb84ab9b1b8
mqi-cs <mqasimimran@outlook.com>
2025-03-11 12:41:40 +0000

    Draw block with more stuff like background changes in

--- Wordle.py
+++ Wordle.py
@@ -1,7 +1,15 @@
 
 import pygame  #importing required libraries
 
+
 screen_width,screen_height = 600,700    #variables for screen width and height
+
+# Define colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+GREY = (128, 128, 128)
 
 screen = pygame.display.set_mode((screen_width,screen_height))   #initialising display window
 
@@ -17,6 +25,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill(WHITE)  # Fill the screen with white
+
+    # Draw a black rectangle
+    pygame.draw.rect(screen, BLACK, (100, 100, 200, 100))  # (x , y cooridnates, width, height)
+
     pygame.display.flip()  # Update the screen
 
-pygame.quit()
+pygame.quit() # Quit the game
