730ae9638b1e4731243ee548729b5e66e8eb0ab6
mqi-cs <mqasimimran@outlook.com>
2025-03-11 11:52:23 +0000

    redo

---
+++ Wordle.py
@@ -0,0 +1,20 @@
+
+import pygame  #importing required libraries
+
+screen_width,screen_height = 600,700    #variables for screen width and height
+
+screen = pygame.display.set_mode((screen_width,screen_height))   #initialising display window
+
+pygame.display.set_caption("Wordle Variant")           #window name
+ 
+
+pygame.init()           #initialising pygame
+
+
+running = True                    #main loop allowing to exit app
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
